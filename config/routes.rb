d6a5b19bad1f3b71ac41661152ee06f09ab424d7
Tommy <tjoa@siscotelcloud.com>
2024-02-05 17:28:42 -0500

    branching to coinapi

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+  get 'static_pages/home'
+
+  #Root Page
+  root to: 'static_pages#home'
+end
